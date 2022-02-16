c63f15419057f6afb1a3b8c419f7dea0520c613e
crclz <crucialize@foxmail.com>
2022-02-16 19:45:58 +0800

    change version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="rightchain",
-    version="0.0.5",
+    version="0.0.6",
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
