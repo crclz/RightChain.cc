93d067b104deb26ee03709734c6b28519a1a4f0a
crclz <crucialize@foxmail.com>
2022-02-25 18:51:59 +0800

    test list_all_files_in_workdir

---
+++ src/rightchain_test/infra/file_lister_test.py
@@ -0,0 +1,21 @@
+from unittest.mock import Mock
+
+from pathlib2 import Path
+from rightchain.infra.file_lister import FileListerService
+import os
+
+
+def test_list_all_files_in_workdir_happy():
+    # arrange
+    lister = FileListerService(Mock())
+    basePath = Path(__file__).parent / "sample"
+
+    # act
+    savedWorkDir = os.getcwd()
+    os.chdir(basePath)
+    files = lister.list_all_files_in_workdir()
+    os.chdir(savedWorkDir)
+
+    # assert
+
+    assert set(files) == {"./1.hello", "./2/3.hello"}
