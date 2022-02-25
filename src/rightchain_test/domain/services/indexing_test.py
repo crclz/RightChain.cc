8ae4297460f9632d2afae8804e13892b339b52d7
crclz <crucialize@foxmail.com>
2022-02-25 15:06:30 +0800

    test_generateSalt

--- src/rightchain_test/domain/services/indexing_test.py
+++ src/rightchain_test/domain/services/indexing_test.py
@@ -9,8 +9,8 @@ def test_fileSha256():
     indexingService = IndexingService(Mock(), Mock(), Mock())
 
     expectedSha256 = "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4"
-    
-    file = pathlib.Path(__file__).parent / 'sample.txt'
+
+    file = pathlib.Path(__file__).parent / "sample.txt"
 
     # act
     result = indexingService.fileSha256(file.__str__())
@@ -18,14 +18,28 @@ def test_fileSha256():
     # assert
     assert expectedSha256 == result
 
+
 def test_stringSha256():
     # arrange
     indexingService = IndexingService(Mock(), Mock(), Mock())
 
     expectedSha256 = "dffd6021bb2bd5b0af676290809ec3a53191dd81c7f70a4b28688a362182986f"
-    
+
     # act
     result = indexingService.stringSha256("Hello, World!")
 
     # assert
     assert expectedSha256 == result
+
+
+def test_generateSalt():
+    # arrange
+    indexingService = IndexingService(Mock(), Mock(), Mock())
+
+    # act
+    result = indexingService.generateSalt(5)
+
+    # assert
+    assert isinstance(result, str)
+    assert 10 == len(result)
+
